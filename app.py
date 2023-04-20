af831c2d601718a21ee6866464ad15216de91e79
nandu-k01 <nandagopan.k01@gmail.com>
2023-04-20 19:39:31 +0530

    final commit

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
+
 nltk.download('punkt')
 nltk.download('stopwords')
 
 app = Flask(__name__, template_folder='templates')
 ps = PorterStemmer()
+
+# Load the trained model and vectorizer from pickle files during startup
+with open('vectorizer.pkl', 'rb') as f:
+    tfidf = pickle.load(f)
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 @app.route('/')
 def home():
@@ -16,49 +24,40 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    tfid = pickle.load(open('vectorizer.pkl', 'rb'))
-    model = pickle.load(open('model.pkl', 'rb'))
-
-    input_sms = request.form['input_sms']
-    if input_sms == '' :
-        return render_template('index.html', error='{a}'.format('Error'))
-    else:
-        transform_sms = transform_text(input_sms)
-
-        vectorized_sms = tfid.transform(transform_sms)
-
-        result = model.predict(vectorized_sms)[0]
-
-        if result == 1:
-            output = 'Spam'
-        else:
-            output = 'Not Spam'
-
-        return render_template('index.html', prediction_text='{}'.format(output))
+    try:
+        input_sms = request.form['input_sms']
+        if not input_sms:
+            raise ValueError('Please enter a SMS message')
+        
+        # Preprocess the input SMS text
+        text = transform_text(input_sms)
+        
+        # Transform the preprocessed text using the loaded vectorizer
+        vectorized_text = tfidf.transform(text)
+        
+        # Predict the class label using the loaded model
+        predicted_class = model.predict(vectorized_text)[0]
+        
+        # Map the predicted class label to a human-readable output message
+        output_message = 'Spam' if predicted_class == 1 else 'Not Spam'
+        
+        return render_template('index.html', prediction_text=output_message)
+    
+    except ValueError as e:
+        error_message = str(e)
+        return render_template('index.html', error=error_message)
 
 def transform_text(text):
-    text = text.lower()
-    text = nltk.word_tokenize(text)
-
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
-
-    return y
+    # Tokenize the text
+    tokens = nltk.word_tokenize(text.lower())
+    
+    # Remove stopwords and punctuation, and stem the words
+    stopwords_set = set(stopwords.words('english'))
+    punctuation_set = set(string.punctuation)
+    stemmed_tokens = [ps.stem(token) for token in tokens 
+                      if token not in stopwords_set and token not in punctuation_set]
+    
+    return stemmed_tokens
 
 if __name__ == '__main__':
     app.run()
